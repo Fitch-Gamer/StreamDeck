a985923e4da31a8dc1e130502ed0155f8bea767e
Finley-Hitchings <fitchings@outlook.com>
2023-06-03 16:57:50 +0100

    Development
    added ability to change between pages and some ease of use

--- StreamDeck/Form1.cs
+++ StreamDeck/Form1.cs
@@ -17,6 +17,7 @@ namespace StreamDeck
 {
     public partial class Deck : Form
     {
+        public int borderx = 10, bordery = 10, butborderx = 20, butbordery = 20, butx = 100, buty = 100;
         public Deck()
         {
             InitializeComponent();
@@ -24,7 +25,7 @@ namespace StreamDeck
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            Main(10,10,20,20,100,100,"Test.json");
+            Main(borderx,bordery,butborderx,butbordery,butx,buty,"Test.json");
         }
 
         private void Main(int BorderWidthX,int BorderWidthY,int ButBorderWidthX,int ButBorderWidthY, int ButSizeX, int ButSizeY, string StartLoc)
@@ -44,6 +45,7 @@ namespace StreamDeck
                     b.Location = new Point(BorderWidthX + ((ButBorderWidthX + ButSizeX) * x), BorderWidthY + ((ButBorderWidthY + ButSizeY) * y));
                     b.BackgroundImageLayout = ImageLayout.Zoom;
                     b.BackgroundImage = Image.FromFile(BP.Img);
+                    b.Tag = "BP";
                     b.Enabled = true;
                     b.Visible = true;
                     b.Click += (sender,EventArgs) => { ButtonLink(BP.Type, BP.Link);};
@@ -69,6 +71,17 @@ namespace StreamDeck
 
                 case "executable":
                     Process.Start(Link);
+                    break;
+                case "link":
+                    List<Button> ct = Controls.OfType<Button>().ToList();
+                    foreach(Button b in ct)
+                    {
+                        if(b.Tag.ToString() == "BP")
+                        {
+                            b.Dispose();
+                        }
+                    }
+                    Main(borderx, bordery, butborderx, butbordery, butx, buty, Link);
                     break;
 
                 default:
